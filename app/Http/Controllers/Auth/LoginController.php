20fdb87866ac98a6b8b3fdf37c3873dd97f11469
graykode <nlkey2022@gmail.com>
2016-11-11 14:21:00 +0900

    다시 커밋

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+	
+	// 이메일이 아닌 회원 아이디로 로그인
+	public function username()
+	{
+		return 'userid';
+	}
+	
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // http://stackoverflow.com/questions/35675868/laravel-5-2-authentication-error 참조
+		
+        if ($lockedOut = $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        $credentials = $this->credentials($request);
+
+		// 0이 승락 안됨 1이 승락됨
+        $credentials['confirm']=1;
+
+        if ($this->guard()->attempt($credentials, $request->has('remember'))) {
+            return $this->sendLoginResponse($request);
+        }
+
+        if (! $lockedOut) {
+            $this->incrementLoginAttempts($request);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+
+	}
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="col-md-8 col-md-offset-2">
+	<div id="welcome">
+		회원가입이 완료되었습니다.
+	</div>
+	<div style="text-align: center; color:white; margin-top:50px;">
+		로그인이 되지 않나요?</br>운영진이 가입 승인을 할 때까지 잠시만 기다려주세요!</br></br>
+			2016년도 2학기 회장님 연락처 입니다</br>
+			<span id="numberbox">13</span>&nbsp;강준후&nbsp;&nbsp;
+			<i class="fa fa-phone" aria-hidden="true"></i>&nbsp;010-9690-7916
+		</br></br>
+			2016년도 2학기 부회장님 연락처 입니다</br>
+			<span id="numberbox">16</span>&nbsp;강수빈&nbsp;&nbsp;
+			<i class="fa fa-phone" aria-hidden="true"></i>&nbsp;010-7732-8831
+	</div>
+</div>
+@endsection
